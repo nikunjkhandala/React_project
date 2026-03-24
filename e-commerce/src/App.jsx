c063200f5050ddc6681e488cbd2068baf0226520
nikunjkhandala <khandalanikunj286@gmail.com>
2026-03-24 17:50:24 +0530

    error code

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Women from "./pages/Women";
 import Men from "./pages/Men";
+import Kids from "./pages/Kids";
+import Shop from "./pages/Shop";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Shop />} />
         <Route path="/men" element={<Men />} />
         <Route path="/women" element={<Women />} />
-        <Route path="kids" element={<Kids />} />
+        <Route path="/kids" element={<Kids />} />
       </Routes>
     </div>
   );
--- e-commerce/src/components/Navbar.jsx
+++ e-commerce/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import Shop from "../pages/Shop";
 import Men from "../pages/Men";
 import Women from "../pages/Women";
 import Kids from "../pages/Kids";
+import { a } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -16,16 +17,16 @@ const Navbar = () => {
         <div>
           <ul className="flex gap-10 items-center py-4">
             <li>
-              <NavLink to="/">Shop</NavLink>
+              <a href="/">Shop</a>
             </li>
             <li>
-              <NavLink to="/men">Men</NavLink>
+              <a href="/men">Men</a>
             </li>
             <li>
-              Women <NavLink to="/women">Women</NavLink>
+              <a href="/women">Women</a>
             </li>
             <li>
-              <NavLink to="/kids">kids</NavLink>
+              <a href="/kids">Kids</a>
             </li>
           </ul>
         </div>
